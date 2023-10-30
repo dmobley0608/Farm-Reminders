7af6408bd7866612fe770b4d51f87b59665bc3d3
dmobley0608 <dmobley0608@gmail.com>
2023-10-30 10:18:52 -0400

    bad change

--- main.py
+++ main.py
@@ -1,138 +1,61 @@
-import psycopg
+import requests
 from classes.horse import Horse
 from classes.record import  Record
-from email.message import EmailMessage
-import ssl
-import smtplib
+from classes.email import Email
 from datetime import datetime
 import time
 url = 'db.tgdqkumgqyptsqjmlwkf.supabase.co'
-
-
-
-    
                 
-google_key = 'tons bame pkmc vgxq'
-email_sender = 'dmobley0608@gmail.com'
-email_reciever = 'tmobley@hallcounty.org'
-subject = "DD Cattle"
-body = f"""
-<img src='https://ddcattle.company/static/media/ddc.b51fd10ab57a812f22d9.png' width='200px'/>
-<h1>Vaccination Status and Worming Updates</h1>
-<h2>The following horses need to be wormed:</h2>
-<ol>
-<li>
-    {'<li>'.join(worming)}
-</ol>
-<hr>
-<h2>The following horses need Coggins pulled:</h2>
-<ol>
-<li>
-    {'<li>'.join(coggins)}
-</ol>
-<hr>
-<h2>The following horses need rabies shots:</h2>
-<ol>
-<li>
-    {'<li>'.join(rabies)}
-</ol>
-<hr>
-<h2>The following horses need yearly vaccines:</h2>
-<ol>
-<li>
-    {'<li>'.join(vaccines)}
-</ol>
-<hr>
-"""
-welcome_message =  f"""
-<img src='https://ddcattle.company/static/media/ddc.b51fd10ab57a812f22d9.png' width='200px'/>
-<h1>You have been enrolled to recieve DDCATTLE'S Vaccination Status and Worming Updates</h1>
-"""
-em = EmailMessage()
-em['From'] = 'ddcattle company'
-em['To'] = email_reciever
-em.add_header('Content-Type','text/html')
-em['Subject'] = subject
-em.set_payload(welcome_message)
 
-context = ssl.create_default_context()
-print("Sending Initial Email")
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            smtp.login(email_sender, google_key)
-            smtp.sendmail(email_sender, email_reciever,em.as_string())
+
 
 
 while True: 
     count = 0   
-    horses = {}
-    records = []
+    horses = []    
     worming = []
     coggins = []
     vaccines = []
     rabies = []
-    
+    print("-"*150)
     print(f"Checking Records current time is {datetime.now()}")  
-    #Fetch Data
-    print("Fetching Records")
-    with psycopg.connect(f'host={url} user=postgres password=thebigdawgisawesome! port=5432') as conn:
-        with conn.cursor() as cur:
-            cur.execute('Select name, date, wormed, coggins, rabies, yearly_vaccines, off_property_rides FROM medical_records JOIN horses ON horses.id = medical_records.horse_id')
-            response = cur.fetchall()
-            for record in response:           
-                if not horses.get(record[0]):
-                    horses[record[0]] = Horse(record[0], record[6])
-                records.append(record)
-                count += 1
-    print(f"{count} Records")
-    #Organize Records By Horse
-    for record in records:
-        if horses[record[0]].name == record[0]:
-            horses[record[0]].add_record(Record(horse=horses.get(record[0]),date=record[1],rabies= record[4],coggins= record[3],wormed= record[2],yearly_vaccines= record[5]))
+    #Fetch Data   
+    r = requests.get('https://ddcattle.company/api/horses')
+    data = r.json()
+    print(f'Record Check Complete')
+    for horse in data:
+        horses.append(Horse(horse['name'], horse['MedicalRecords'], horse['off_property_rides']))
+          
     #Check Record Dates
-    for horse in horses.values():    
-        if horse.check_record('coggins',12, 9) and horse.off_property_rides:
-            coggins.append(horse.name)
-        if horse.check_record('rabies',12, 9):
-            rabies.append(horse.name)
-        if horse.check_record('wormed',4, 4):
-            worming.append(horse.name)
-        if horse.check_record('yearly_vaccines',12, 9):
-            vaccines.append(horse.name)
-            
-    #Set Body For Email
-    body = f"""
-        <img src='https://ddcattle.company/static/media/ddc.b51fd10ab57a812f22d9.png' width='200px'/>
-        <h1>Vaccination Status and Worming Updates</h1>
-        <h2>The following horses need to be wormed:</h2>
-        <ol>
-        <li>
-            {'<li>'.join(worming)}
-        </ol>
-        <hr>
-        <h2>The following horses need Coggins pulled:</h2>
-        <ol>
-        <li>
-            {'<li>'.join(coggins)}
-        </ol>
-        <hr>
-        <h2>The following horses need rabies shots:</h2>
-        <ol>
-        <li>
-            {'<li>'.join(rabies)}
-        </ol>
-        <hr>
-        <h2>The following horses need yearly vaccines:</h2>
-        <ol>
-        <li>
-            {'<li>'.join(vaccines)}
-        </ol>
-        <hr>
-"""
-    em.set_payload(body)
-    # Send Email with results
+    for horse in horses: 
+        #Check For Coggins
+        count += len(horse.records)
+        records = horse.check_records()
+        coggins.append(records.get('coggins') or  )
+        rabies.append(records.get('rabies'))
+        vaccines.append(records.get('vaccines'))
+        worming.append(records.get('wormed'))
+    print(coggins)
+  
+   
+    #Print status
+    print(f'Checked {count} Records')
+    print(f'{len(coggins)} - Horses Need Coggins')
+    print(f'{len(worming)} - Horses Need To Be Wormed')
+    print(f'{len(vaccines)} - Horses Need Yearly Vaccines')
+    print(f'{len(rabies)} - Horses Need A Rabies Shot')
+    # Send Email with results   
     if len(coggins) > 0 or len(rabies) > 0 or len(worming) > 0 or len(vaccines) > 0:
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            smtp.login(email_sender, google_key)
-            smtp.sendmail(email_sender, email_reciever, em.as_string()) 
-    # Wait an Hour and Do it again
-    time.sleep(3600)        
+        email = Email("Vaccination/Worming Status")
+        email.add_recipient('dmobley0608@gmail.com')
+        email.add_recipient('djones@hallcounty.org')
+        # email.set_body_for_vaccines(coggins, rabies, vaccines, worming)
+        # email.send_email()
+        print(email.body)
+        
+    else:
+        print("All records up to date")
+    print("Going To Sleep. I will check again tomorrow!")
+    print("-"*150)
+    # Wait One Day and Do it again
+    time.sleep(86400)        
